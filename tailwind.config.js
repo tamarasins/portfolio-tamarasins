10c451fa2ae30a907e07ff1935f6346c5eb6ef47
Tamara <tam.maier97@gmail.com>
2025-11-11 14:36:44 +0100

    Was auch immer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,27 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx,css}', // CSS hinzufügen!
+    './components/**/*.{js,ts,jsx,tsx,css}',
   ],
   darkMode: 'class',
   theme: {
     extend: {
       colors: {
-      'primary-bg': '#FDE8C4',
-      'highlight': '#E56D6A',
-      'block': '#FFFFFF',
+        primaryBg: '#FDE8C4',
+        highlight: '#E56D6A',
+        block: '#FFFFFF',
       },
       fontFamily: {
         heading: ['Quicksand', 'sans-serif'],
         body: ['Inter', 'sans-serif'],
       },
       boxShadow: {
-        'block': '0 10px 25px rgba(0,0,0,0.1)',
-        'menu': '0 5px 15px rgba(0,0,0,0.15)',
+        block: '0 10px 25px rgba(0,0,0,0.1)',
+        menu: '0 5px 15px rgba(0,0,0,0.15)',
       },
       borderRadius: {
-        'menu': '1.5rem',
-        'block': '1rem',
+        menu: '1.5rem',
+        block: '1rem',
       },
     },
   },
